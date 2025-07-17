f32484f88701a39c052a46f4d8a0ecab05330562
Harshil Patel <harshilp05012580@gmail.com>
2025-07-17 16:04:44 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,27 @@
+print("1-Addition")
+print("3-Subtraction")
+print("3-Multiplication")
+print("4-Division")
+operation=int(input("Choose your operation:"))
+num1=float(input("Enter your first number:"))
+num2=float(input("Enter your second number:"))
+
+if (operation in [1,2,3,4]):
+    
+    if(operation == 1):
+        result=num1+num2
+    
+    elif(operation == 2):
+        result=num1-num2
+    
+    elif(operation == 3):
+        result=num1*num2
+    
+    elif(operation == 4):
+        if(num2 != 0):
+            result=num1/num2
+        else:
+            print("Can not divide by 0")
+else:
+    print("Invalid Operation!")
+print("The result of your operation is {}".format(result))
